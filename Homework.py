44738d5aaab7deddba0392b92e0db59d53095434
StasiliusD <dmitriev.stan@yandex.ru>
2022-07-05 20:42:11 +0300

    created new python file

---
+++ Homework.py
@@ -0,0 +1,8 @@
+class Stas:
+    def Eat(meal):
+        print(f'{meal} - GIVE ME THAT')
+    def Sleep(tiredness = 8):
+        print('I"ll sleep for {tiredness} hours')
+a = Stas()
+a.Eat('potato')
+a.Sleep(10)            
